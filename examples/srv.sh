fc8190ae3ed5012b242c26e1ead52c8431081837
Alexey Melezhik <melezhik@gmail.com>
2026-01-08 17:35:49 +0300

    new check

---
+++ examples/srv.sh
@@ -0,0 +1,21 @@
+session=$(date +%s)
+
+sudo systemctl is-enabled knot 2>&1 | dtap --box - \
+--session $session \
+--check srv-enabled \
+--desc "knot srv enable"
+
+
+sudo systemctl is-active knot 2>&1 | dtap --box - \
+--session $session \
+--check srv-enabled \
+--desc "knot srv active"
+
+dtap  --report  --session $session
+
+ex_code=$?
+
+if [[ ex_code -eq 1 ]]; then
+    echo
+    dtap --report --details --failures --session $session
+fi
