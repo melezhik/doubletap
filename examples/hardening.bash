1bda9ecf4e45dc4dfec5bd4933c6e1f2a331642f
Alexey Melezhik <melezhik@gmail.com>
2026-07-15 21:03:11 +0300

    Create hardening.bash

---
+++ examples/hardening.bash
@@ -0,0 +1,9 @@
+export PATH=$PWD:$PATH
+
+session=$(date +%s)
+
+sudo firewall-cmd --state | dtap \
+--check firewall-default-deny \
+--box - \
+--session $session \
+--desc "firewall default policy is deny"
